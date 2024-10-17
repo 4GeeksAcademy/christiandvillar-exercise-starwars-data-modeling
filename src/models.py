4ca91bff3df7738c9d4537d753ea9fd33900ef27
Christian David Villar Colodro <86432506+ChristianDVillar@users.noreply.github.com>
2024-10-17 13:28:24 +0000

    [model star wars]

--- src/models.py
+++ src/models.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
@@ -7,26 +6,76 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
 class Person(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    height = Column(String(250))
+    mass = Column(String(250))
+    hair_color = Column(String(250))
+    skin_color = Column(String(250))
+    eye_color = Column(String(250))
+    birth_year = Column(String(250))
+    gender = Column(String(50))
+    homeworld_id = Column(Integer, ForeignKey('planet.id'))
+    
+    homeworld = relationship("Planet")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    diameter = Column(String(250))
+    rotation_period = Column(String(250))
+    orbital_period = Column(String(250))
+    gravity = Column(String(250))
+    population = Column(String(250))
 
-    def to_dict(self):
-        return {}
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250))
+    starship_class = Column(String(250))
+    manufacturer = Column(String(250))
+    cost_in_credits = Column(String(250))
+    length = Column(String(250))
+    crew = Column(String(250))
+    passengers = Column(String(250))
+    max_atmosphering_speed = Column(String(250))
+    hyperdrive_rating = Column(String(250))
+    MGLT = Column(String(250))
+    cargo_capacity = Column(String(250))
+    consumables = Column(String(250))
 
-## Draw from SQLAlchemy base
+# Tabla intermedia para Favoritos (Usuario puede tener varios favoritos de distintos tipos)
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    person_id = Column(Integer, ForeignKey('person.id'), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    starship_id = Column(Integer, ForeignKey('starship.id'), nullable=True)
+
+    user = relationship("User", back_populates="favorites")
+    person = relationship("Person")
+    planet = relationship("Planet")
+    starship = relationship("Starship")
+
+User.favorites = relationship("Favorite", back_populates="user")
+
+# Crear el motor de la base de datos
+engine = create_engine('sqlite:///starwars_blog.db')
+
+# Crear todas las tablas
+Base.metadata.create_all(engine)
+
+# Dibujar el diagrama ER
 render_er(Base, 'diagram.png')
+
